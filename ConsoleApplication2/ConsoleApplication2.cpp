fc89a7087eca8472c195fd04118c1c22f920e7f4
jakubs2405 <kuba_str@poczta.fm>
2025-10-27 16:33:46 +0100

    Add project files.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,100 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//zad 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //for (int i = 10; i >= 0; i--) {
+        //cout << i << endl;
+    //}
+
+
+
+    //zad2,3
+    //int fahr, celsius;
+    //int start, limit, krok;
+
+    //start = 0;
+    //limit = 200;
+    //krok = 20;
+
+    //for (fahr = 0.0; fahr <= limit; fahr = fahr + krok) {
+        //float celsius = (5.0 / 9.0) * (fahr - 32.0);
+        //cout << fahr << "\t" << celsius << endl;
+    //}
+
+    //zad5
+    //int fahr, celsius;
+    //int start, limit, krok;
+
+    //start = 0;
+    //limit = 200;
+   // krok = 20;
+
+    //for (fahr = 0.0; fahr <= limit; fahr = fahr + krok) {
+        //float celsius = (5 / 9) * (fahr - 32.0);
+        //cout << fahr << "\t" << celsius << endl;
+    //}
+
+    //zad 6
+    //int fahr, celsius;
+    //int start, limit, krok;
+    //start = 0;
+
+    //cout << "wartosc koncowa: ";
+    //cin >> limit;
+    //cout << "krok: ";
+    //cin >> krok;
+
+   // for (fahr = 0.0; fahr <= limit; fahr = fahr + krok) {
+    // float celsius = (5.0 / 9.0) * (fahr - 32.0);
+        //cout << fahr << "\t" << celsius << endl;
+    //}
+    //zad 7
+    
+     
+    //zad 8
+    
+    //for (int i = 0; i <=100; i = i +2) {
+        //cout << i << endl;
+    //}
+
+    //zad9
+    int n; 
+    cout << "
+
+    
+    
+    
+
+
+     
+    
+      
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        return 0;
+}
